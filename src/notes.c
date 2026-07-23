585bc7a4008fb82c4572e4e622c2106c47dce071
Graywolf857 <graysonjones857@gmail.com>
2026-07-23 12:26:23 -0700

    req changes

---
+++ src/notes.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "notes.h"
+
+
+int notes_add(const char *filename,
+              const char *title,
+              const char *body){
+
+    
+
+    return;
+}
